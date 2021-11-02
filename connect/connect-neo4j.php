5340b546432fede1a1bc35f597fa7529ab7902f9
zhdanov <yuriy.zhdanov@gmail.com>
2021-11-02 12:34:20 +0300

    fix(connect): remove trash

--- connect/connect-neo4j.php
+++ connect/connect-neo4j.php
@@ -3,8 +3,6 @@
 use Neoxygen\NeoClient\ClientBuilder;
 
 try {
-    $dbh_bank = new PDO('pgsql:host=localhost;dbname=' . $config['bank']['dbname'], $config['bank']['user'], $config['bank']['password']);
-
     $clt_neo4j = ClientBuilder::create()
     ->addConnection('default','http','localhost',7474,true, $config['neo4j']['user'], $config['neo4j']['password'])
     ->setAutoFormatResponse(true)
