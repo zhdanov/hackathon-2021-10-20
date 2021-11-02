763b07fbee5994b2a267a7fc4fd7a17915640755
zhdanov <yuriy.zhdanov@gmail.com>
2021-11-02 13:08:05 +0300

    fix(neo4j): connect

--- install.bash
+++ install.bash
@@ -3,10 +3,11 @@
 DB_USER=gbc
 DB_PASSWORD=qWe12_34
 
+sudo apt -y update
 sudo apt -y install vim
 sudo apt -y install net-tools
-sudo apt -y install php7.4-cli php-pgsql php-mysqli php-zip php-mbstring php-curl
-sudo apt -y install wget php-zip unzip
+sudo apt -y install php-cli php-pgsql php-mysqli php-mbstring php-curl php-zip unzip
+sudo apt -y install wget
 
 # postgres
 sudo -u postgres -i psql -d bank -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD'"
@@ -20,6 +21,10 @@ sudo -u postgres -i psql -d bank -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEM
 sudo -u root -i mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD'"
 sudo -u root -i mysql -e "GRANT ALL PRIVILEGES ON * . * TO '$DB_USER'@'localhost'"
 sudo -u root -i mysql -e "FLUSH PRIVILEGES"
+
+# neo4j
+sudo sed -i "/.*#dbms\.security\.auth_enabled.*/c\dbms\.security\.auth_enabled=false" /etc/neo4j/neo4j.conf
+sudo systemctl restart neo4j
 
 # composer
 wget https://getcomposer.org/installer -O composer-setup.php
